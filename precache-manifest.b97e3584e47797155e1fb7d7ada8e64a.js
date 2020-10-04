e4c6cf026694563045402b05f0aeaf53bbf10e37
LYNN DAMOTA <bdamota0506@gmail.com>
2020-10-03 23:55:00 -0700

    Updates

---
+++ precache-manifest.b97e3584e47797155e1fb7d7ada8e64a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13ea235083e1153615dd11cfec4f665d",
+    "url": "/bdamota-portfolio-project/index.html"
+  },
+  {
+    "revision": "88c487a4df9c4489798f",
+    "url": "/bdamota-portfolio-project/static/css/main.dae3a5ce.chunk.css"
+  },
+  {
+    "revision": "5f37ac208b8ad1e2fd43",
+    "url": "/bdamota-portfolio-project/static/js/2.072167c4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/bdamota-portfolio-project/static/js/2.072167c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88c487a4df9c4489798f",
+    "url": "/bdamota-portfolio-project/static/js/main.99e87bf1.chunk.js"
+  },
+  {
+    "revision": "d5117ab29b1394fac22a",
+    "url": "/bdamota-portfolio-project/static/js/runtime-main.c507b6d8.js"
+  },
+  {
+    "revision": "2b13656a9dbe8d7000a834f795ccc15a",
+    "url": "/bdamota-portfolio-project/static/media/Notetaker.2b13656a.png"
+  },
+  {
+    "revision": "4fe5776be46e2f3f6dbf6a9db306a57a",
+    "url": "/bdamota-portfolio-project/static/media/cover-image.4fe5776b.jpg"
+  },
+  {
+    "revision": "ae013ec264eb730e86389f7ce6cd53b5",
+    "url": "/bdamota-portfolio-project/static/media/header-background.ae013ec2.jpg"
+  },
+  {
+    "revision": "df2cbaa9a2088cd9954698ebbc2537a3",
+    "url": "/bdamota-portfolio-project/static/media/kickback.df2cbaa9.png"
+  },
+  {
+    "revision": "511a86c6c3f1d3cb9b82358df72a2928",
+    "url": "/bdamota-portfolio-project/static/media/mylingual.511a86c6.png"
+  },
+  {
+    "revision": "655b4b66fd24c1c0ce6f06ffd1b059a1",
+    "url": "/bdamota-portfolio-project/static/media/passwordgen.655b4b66.png"
+  },
+  {
+    "revision": "d348c720e048fe860f79c0693e13885a",
+    "url": "/bdamota-portfolio-project/static/media/weather.d348c720.png"
+  },
+  {
+    "revision": "8ecd7078438222e72521fb51760efd1e",
+    "url": "/bdamota-portfolio-project/static/media/zookeeper.8ecd7078.png"
+  }
+]);
